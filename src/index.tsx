223c95bc2dbbb897eca662af96bf7b8d3c953fe5
dennisappiahALX <appiahdennis1998@gmail.com>
2023-03-12 04:03:54 +0000

    img

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Products provider
+import ProductProvider from './context/ProductsContext';
+// sideBarProvider
+import SideBarProvider from './context/SideBarContext';
+//cartsProvider
+import CartProvider from './context/CartsContext';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <CartProvider>
+    <SideBarProvider>
+    <ProductProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ProductProvider>
+  </SideBarProvider>
+  </CartProvider>
+  
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
